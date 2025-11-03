4842ff54e58da8878b8fedf2f2259de19b7629ef
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-11-03 23:06:16 +0530

    feat: add login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (err) {
+      console.log("Error: ", err.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-[20%]">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email Id</legend>
+              <input
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+                type="text"
+                className="input"
+                placeholder="Type here"
+              />
+            </fieldset>
+
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="text"
+                className="input"
+                placeholder="Type here"
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button onClick={handleLogin} className="btn btn-primary btn-md">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
