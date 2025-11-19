ac9e4107a146006512f950f8014072495ba3f330
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-11-19 22:13:04 +0530

    feat: updated backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "http://192.168.1.4:3000";
+    : "https://devmate-backend-fheq.onrender.com";
